de916699deef148ebad44bf9d88c545d70a9bd2e
Albert Dixon <adixon415@gmail.com>
2021-11-12 12:54:25 -0800

    feat(*): Add more config options and bStats config template (#5)

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-/bin/gomplate --file=/velocity/velocity.toml.tpl --out=/velocity/velocity.toml
+if ! [[ -f /velocity/velocity.toml ]] || [[ $FORCE_OVERWRITE_CONFIGS =~ ^[tT](rue|RUE)|[yY](es|ES)|1$ ]]; then
+  /bin/gomplate --file=/velocity/velocity.toml.tpl --out=/velocity/velocity.toml
+fi
+
+if ! [[ -f /velocity/plugins/bStats/config.txt ]] || [[ $FORCE_OVERWRITE_CONFIGS =~ ^[tT](rue|RUE)|[yY](es|ES)|1$ ]]; then
+  /bin/gomplate --file=/velocity/plugins/bStats/config.txt.tpl --out=/velocity/plugins/bStats/config.txt
+fi
+
 cd /velocity || exit
 exec /velocity/start-velocity.sh
